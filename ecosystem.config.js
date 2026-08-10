6c325af53d087967c878fbbd0283fb4d88200611
nareshkannasln <nareshkannashanmugam@gmail.com>
2026-08-10 11:57:20 +0530

    feat: add odisha prod backend url

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,6 +1,18 @@
 module.exports = {
   apps: [
     {
+      name: 'odisha-production',
+      cwd: '/home/deploy/apps/nomination-web',
+      script: 'node_modules/next/dist/bin/next',
+      interpreter: '/home/deploy/.nvm/versions/node/v20.20.1/bin/node',
+      args: 'start -p 3003',
+      env_production: {
+        NODE_ENV: 'production',
+        PORT: 3003,
+        BACKEND_URL: 'https://rdb-odisha-prod.m.frappe.cloud/',
+      },
+    },
+    {
       name: 'odisha-staging',
       cwd: '/home/deploy/apps/nomination-web-odisha',
       script: 'node_modules/next/dist/bin/next',
